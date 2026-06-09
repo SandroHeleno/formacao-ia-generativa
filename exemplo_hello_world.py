02895d5e9e7d6ae0c1de0eff5103c406c6697e7c
SandroHeleno <seu.email@exemplo.comgit>
2026-06-09 19:27:34 -0300

    Mensagem sobre o curso

--- exemplo_hello_world.py
+++ exemplo_hello_world.py
@@ -1,3 +1,5 @@
 print("Hello World1")
 idade = 25
-print("Minha idade é:", idade)    
+print("Minha idade é:", idade)  
+curso = "Formação IA Generativa"
+print("Estou fazendo o curso de:", curso)  
